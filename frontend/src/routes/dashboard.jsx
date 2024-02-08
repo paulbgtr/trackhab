5bbf9417b4803e2bc5a6e4e7cf370dd5750d1936
paulbg <paulbg@hey.com>
2024-02-08 13:47:33 +0200

    feat: add HabitList to dashboard

--- frontend/src/routes/dashboard.jsx
+++ frontend/src/routes/dashboard.jsx
@@ -1,11 +1,13 @@
 import { Layout } from "../components/Layout/Layout";
-import { DayTime } from "./DayTime";
+import { DayTime } from "../components/routesComponents/dashboard/DayTime";
+import { HabitList } from "../components/routesComponents/dashboard/HabitList";
 
 const Dashboard = () => {
   return (
     <Layout navbar>
       <h1 className="font-bold text-3xl">Hey, User!</h1>
       <DayTime />
+      <HabitList />
     </Layout>
   );
 };
