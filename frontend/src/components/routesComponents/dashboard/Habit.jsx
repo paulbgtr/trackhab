7bb404ed79c1a1ac9498dec248d2df3500d89410
paulbg <paulbg@hey.com>
2024-02-09 09:20:14 +0200

    feat: change habit view on isDone
    Implement separate isFinishied state which takes the isDone prop and the component, depending on it, changes the view.

--- frontend/src/components/routesComponents/dashboard/Habit.jsx
+++ frontend/src/components/routesComponents/dashboard/Habit.jsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
+
 export const Habit = ({ title, isDone }) => {
+  const [isFinished, setIsFinished] = useState(isDone);
+
+  const handleCheckbox = () => {
+    setIsFinished(!isFinished);
+  };
+
   return (
     <div className={`rounded-xl min-w-xl shadow-md px-7 py-5`}>
       <div className="flex justify-between">
         <div className="my-auto mx-4">
           <h3
             className={`${
-              isDone && "line-through text-gray-500"
+              isFinished && "line-through text-gray-500"
             } font-bold text-xl`}
           >
             {title}
           </h3>
         </div>
-        <input type="checkbox" className="checkbox my-auto checkbox-lg" />
+        <input
+          onClick={handleCheckbox}
+          type="checkbox"
+          className="checkbox my-auto checkbox-lg"
+        />
       </div>
     </div>
   );
