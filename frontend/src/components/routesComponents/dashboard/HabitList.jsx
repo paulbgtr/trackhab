4c6f4d21cb38a57459c17424b8bedb98492fad52
paulbg <paulbg@hey.com>
2024-02-09 08:46:59 +0200

    feat: add NewHabit to HabitList

--- frontend/src/components/routesComponents/dashboard/HabitList.jsx
+++ frontend/src/components/routesComponents/dashboard/HabitList.jsx
@@ -1,10 +1,12 @@
 import { Habit } from "./Habit";
+import { NewHabit } from "./NewHabit";
 
 export const HabitList = ({ dayTime }) => {
   return (
     <div className="max-w-xl">
-      <div className="grid">
+      <div className="grid gap-3">
         <Habit title="wash" />
+        <NewHabit />
       </div>
     </div>
   );
