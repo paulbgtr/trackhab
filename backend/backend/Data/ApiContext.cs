d612ce358bff4851d5c532d1ab638261a107e0d5
paulbg <paulbg@hey.com>
2024-02-10 13:24:37 +0200

    feat: add ApiContext
    This is a database context to interact with the habits set

---
+++ backend/backend/Data/ApiContext.cs
@@ -0,0 +1,13 @@
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data;
+
+public class ApiContext : DbContext
+{
+    public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Habit> Habits { get; set; }
+}
