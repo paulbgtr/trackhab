1f5acbf3f0197b8996d2f35594783e5dda881d2b
paulbg <paulbg@hey.com>
2024-02-10 13:27:51 +0200

    feat: implement using swagger and DbContext

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,5 +1,21 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApiContext>
+    (opt => opt.UseInMemoryDatabase("trackhab"));
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/", () => "Hello World!");
 
