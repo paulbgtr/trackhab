3e32744e60b5c700dc22e2660e38383cd168c2e4
paulbg <paulbg@hey.com>
2024-02-12 10:15:20 +0200

    feat: implement CRUD for habits

--- backend/backend/Controllers/HabitsController.cs
+++ backend/backend/Controllers/HabitsController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers;
 
-[Route("api/[controller]")]
+[Route("[controller]")]
 [ApiController]
 public class HabitsController : ControllerBase
 {
@@ -15,24 +15,57 @@ public class HabitsController : ControllerBase
         _context = context;
     }
 
-    [HttpPost]
-    public JsonResult CreateEdit(Habit habit)
+    [HttpGet]
+    public JsonResult Get(int habitId)
     {
-        if (habit.Id == 0)
-        {
-            _context.Habits.Add(habit);
-        }
-        else
-        {
-            var habitInDb = _context.Habits.Find(habit.Id);
+        var foundHabit = _context.Habits.Find(habitId);
 
-            if (habitInDb == null) return new JsonResult("Habit not found") { StatusCode = 404 };
+        if (foundHabit == null)
+            return new JsonResult($"Habit with and id of {habitId} is not found") { StatusCode = 404 };
 
-            habitInDb = habit;
-        }
+        return new JsonResult(foundHabit);
+    }
+
+    [HttpPost]
+    public JsonResult Create(Habit habit)
+    {
+        var habitExists = _context.Habits.Find(habit.Id);
+
+        if (habitExists != null) return new JsonResult("Habit already exists") { StatusCode = 400 };
+
+        _context.Habits.Add(habit);
+        _context.SaveChanges();
+
+        return new JsonResult(habit);
+    }
+
+    [HttpDelete]
+    public JsonResult Delete(int habitId)
+    {
+        var habitExists = _context.Habits.Find(habitId);
+
+        if (habitExists == null)
+            return new JsonResult($"Habit with and id of {habitId} is not found") { StatusCode = 404 };
+
+        _context.Habits.Remove(habitExists);
+        _context.SaveChanges();
+
+        return new JsonResult($"Habit with and id of {habitId} has been deleted");
+    }
+
+    [HttpPut]
+    public JsonResult Update(Habit habit)
+    {
+        var habitExists = _context.Habits.Find(habit.Id);
+
+        if (habitExists == null)
+            return new JsonResult($"Habit with and id of {habit.Id} is not found") { StatusCode = 404 };
+
+        habitExists.Title = habit.Title;
+        habitExists.IsDone = habit.IsDone;
 
         _context.SaveChanges();
 
-        return new JsonResult(habit) { StatusCode = 201 };
+        return new JsonResult(habitExists);
     }
 }
