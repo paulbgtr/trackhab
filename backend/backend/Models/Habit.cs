7dd9f62c6a49f11510dc94a0be1da5652db311ac
paulbg <paulbg@hey.com>
2024-02-12 10:31:03 +0200

    feat: implement DaysOfWeek in Habit
    Implement a public enum DayOfWeek and add a new prop of type IList to the Habit model.

--- backend/backend/Models/Habit.cs
+++ backend/backend/Models/Habit.cs
@@ -1,8 +1,20 @@
 namespace backend.Models;
 
+public enum DayOfWeek
+{
+    Sunday = 0,
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6
+}
+
 public class Habit
 {
     public int Id { get; set; }
     public string Title { get; set; }
     public bool IsDone { get; set; }
+    public IList<DayOfWeek> DaysOfWeek { get; set; }
 }
