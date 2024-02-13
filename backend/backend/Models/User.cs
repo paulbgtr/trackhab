82e9e2b3a3367563191c7dbfe372afe45d75241f
paulbg <paulbg@hey.com>
2024-02-13 09:30:54 +0200

    feat: impl setting createdAt automatically in User

--- backend/backend/Models/User.cs
+++ backend/backend/Models/User.cs
@@ -2,6 +2,11 @@ namespace backend.Models;
 
 public class User
 {
+    public User()
+    {
+        CreatedAt = DateTime.Now;
+    }
+
     public int Id { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
