038491db3b1f5d5e14fcceeef09d37f9090618d6
paulbg <paulbg@hey.com>
2024-02-13 09:39:34 +0200

    feat: implement separate enum file for DayOfWeek

--- backend/backend/Models/Habit.cs
+++ backend/backend/Models/Habit.cs
@@ -1,16 +1,5 @@
 namespace backend.Models;
 
-public enum DayOfWeek
-{
-    Sunday = 0,
-    Monday = 1,
-    Tuesday = 2,
-    Wednesday = 3,
-    Thursday = 4,
-    Friday = 5,
-    Saturday = 6
-}
-
 public class Habit
 {
     public int Id { get; set; }
