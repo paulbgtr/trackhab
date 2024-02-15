d045bd55d26507b44a4643e5a9b30cc22d122c06
paulbg <paulbg@hey.com>
2024-02-15 12:55:24 +0200

    feat: update Habit model
    - Add UserId to return a Habit of a corresponding user id.
    - Add DayTime

--- backend/backend/Models/Habit.cs
+++ backend/backend/Models/Habit.cs
@@ -1,9 +1,14 @@
+using backend.Enums;
+using DayOfWeek = System.DayOfWeek;
+
 namespace backend.Models;
 
 public class Habit
 {
     public int Id { get; set; }
+    public int UserId { get; set; }
     public string Title { get; set; }
     public bool IsDone { get; set; }
+    public TimeOfDay DayTime { get; set; }
     public IList<DayOfWeek> DaysOfWeek { get; set; }
 }
