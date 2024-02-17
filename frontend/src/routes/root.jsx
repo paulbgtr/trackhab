94164310931a09af11ef3fa05790beec08260b03
Paul <paulbg@hey.com>
2024-02-17 13:11:12 +0200

    Delete frontend directory

--- frontend/src/routes/root.jsx
+++
@@ -1,7 +0,0 @@
-import { Layout } from "../components/Layout/Layout";
-
-const Root = () => {
-  return <Layout navbar>Home</Layout>;
-};
-
-export default Root;
--- frontend/src/store/useDayTimeStore.js
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand";
-
-export const useDayTimeStore = create((set) => ({
-  selectedTime: "morning",
-  setSelectedTime: (time) => set({ selectedTime: time }),
-}));
